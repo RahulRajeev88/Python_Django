677dbc3c576f73dc7596df575c8b871e2cf00700
Rahul Rajeev <rahulkodinjamkunnel@gmail.com>
2023-07-19 22:13:55 +0530

    else condition updated

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     numbers = {
-        'num1':-10,
+        'num1':10,
 
     }
     return render(request,'index.html',numbers)
