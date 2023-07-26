c26dcd5a04c207dde7bb2b74942af697d9ca4fcb
Rahul Rajeev <rahulkodinjamkunnel@gmail.com>
2023-07-26 22:33:23 +0530

    doctors added

--- home/models.py
+++ home/models.py
@@ -6,9 +6,21 @@ class Departments(models.Model):
     dep_name = models.CharField(max_length=100)
     dep_description = models.TextField()
 
+    def __str__(self):
+        return self.dep_name
+    
+    
+    
+
+
+
 class Doctors(models.Model):
     doc_name = models.CharField(max_length=250)
-    dec_spec = models.CharField(max_length=250)
+    doc_spec = models.CharField(max_length=250)
     dep_name = models.ForeignKey(Departments,on_delete=models.CASCADE)
     doc_image = models.ImageField(upload_to='doctors')
+
+
+    def __str__(self):
+        return self.doc_spec
    
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Departments
+from .models import Departments, Doctors
 
 
 # Create your views here.
@@ -14,7 +14,11 @@ def booking(request):
     return render(request,'booking.html')
 
 def doctors(request):
-    return render(request,'doctors.html')
+    dict_docs = {
+        'doctors':Doctors.objects.all()
+
+    }
+    return render(request,'doctors.html',dict_docs)
 
 def contact(request):
     return render(request,'contact.html')
