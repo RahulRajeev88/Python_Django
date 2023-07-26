c8b58b58bf8a1819d212fe34910750aea86ae42a
Rahul Rajeev <rahulkodinjamkunnel@gmail.com>
2023-07-26 20:08:45 +0530

    Department added

--- home/views.py
+++ home/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Departments
 
 
 # Create your views here.
 def index(request):
-    fruits = {
-        'fruits': ['Banana','Apple','Orange','Mango','Kiwi','Avocado']
-
-    }
-    return render(request,'index.html',fruits)
+    return render(request,'index.html')
 
 def about(request):
     return render(request,'about.html')
@@ -23,4 +20,7 @@ def contact(request):
     return render(request,'contact.html')
 
 def department(request):
-    return render(request,'department.html')
+    dict_dept = {
+        'dept': Departments.objects.all()
+    }
+    return render(request,'department.html',dict_dept)
